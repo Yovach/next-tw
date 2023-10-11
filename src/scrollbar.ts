6b02f03dc2b8d12dfe892dcf7a24e5a9b240d820
Quentin <contact@yovach.dev>
2023-10-11 16:48:14 +0200

    feat: add scrollbar plugin with webkit-* selectors

--- src/scrollbar.ts
+++ src/scrollbar.ts
@@ -1,1 +1,10 @@
-// working on it
+import plugin from "tailwindcss/plugin";
+
+export const scrollbarPlugin = plugin(function ({ addVariant }) {
+  addVariant("scrollbar", "&::-webkit-scrollbar");
+  addVariant("scrollbar-button", "&::-webkit-scrollbar-button");
+  addVariant("scrollbar-thumb", "&::-webkit-scrollbar-thumb");
+  addVariant("scrollbar-track", "&::-webkit-scrollbar-track");
+  addVariant("scrollbar-track-piece", "&::-webkit-scrollbar-track-piece");
+  addVariant("scrollbar-corner", "&::-webkit-scrollbar-corner");
+});
